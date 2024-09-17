f784965a0f5a9d4cdc5807817dda7209315974d0
kovvurisurekha <kovvurisurekha123@gmail.com>
2024-09-17 16:27:34 +0530

    sixth commit

---
+++ colorify/index.js
@@ -0,0 +1,7 @@
+function $43d7963e56408b24$var$paint(color) {
+    const circle = document.getElementById("circleID");
+    circle.style = `background-color:${color}`;
+}
+
+
+//# sourceMappingURL=index.js.map
