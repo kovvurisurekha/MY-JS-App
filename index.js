a56d78aa4f7d34e187748ddcd8a697758d24e8b3
kovvurisurekha <kovvurisurekha123@gmail.com>
2024-09-18 12:03:16 +0530

    commit1

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ function paint(color) {
     circle.style = `background-color:${color}`;
   }
 
-  function deserialize(input) {
-    const { version, data } = JSON.parse(input);
+//   function deserialize(input) {
+//     const { version, data } = JSON.parse(input);
 
-    switch (version) {
-        case '1.0.0':
-            return deserializeV1(data);
-        case '1.1.0':
-            return deserializeV2(data);
-        // Add cases as needed for new versions
-        default:
-            throw new Error(`Unsupported version: ${version}`);
-    }
-}
+//     switch (version) {
+//         case '1.0.0':
+//             return deserializeV1(data);
+//         case '1.1.0':
+//             return deserializeV2(data);
+//         // Add cases as needed for new versions
+//         default:
+//             throw new Error(`Unsupported version: ${version}`);
+//     }
+// }
 
-function deserializeV1(data) {
-    // Handle deserialization for version 1.0.0
-}
+// function deserializeV1(data) {
+//     // Handle deserialization for version 1.0.0
+// }
 
-function deserializeV2(data) {
-    // Handle deserialization for version 1.1.0
-}
+// function deserializeV2(data) {
+//     // Handle deserialization for version 1.1.0
+// }
